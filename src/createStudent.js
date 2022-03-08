ee0f61cbe7285aee671d58f70f979d00d6aa159d
Cristiano Vieira <cristianoviieira@gmail.com>
2022-03-08 19:17:42 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,16 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const boa = () => 'Eita pessoa boa!';
+  const estudante = {
+    name: 'Leandrão, o Lobo Solitário',
+    feedback: boa,
+  };
+  estudante.name = nome;
+  return estudante;
+};
 
 module.exports = createStudent;
+
+// console.log(createStudent('cleiton'));
