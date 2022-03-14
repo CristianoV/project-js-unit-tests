66b06759197c62b6faa41ea94dd2124773b48743
Cristiano Vieira <cristianoviieira@gmail.com>
2022-03-14 00:27:00 -0300

    fundindo a cabeça com os teste

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -24,15 +24,27 @@ const circle = require('../src/circle');
 */
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
+  // it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    // Teste se circle retorna um objeto.
+    it('Testa se circle retorna undefined, caso o parâmetro passado não seja um número.', () => {
+      expect(circle('a')).toBeUndefined();
+    });
+    it('Teste se circle retorna um objeto.', () => {
+      expect(typeof circle(1)).toBe('object')
+    });
     // Teste se o objeto retornado tem 3 propriedades.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se dentro do objeto retornado, a função retorna uma key com value igual a circunferência correta para um círculo de raio 2.
-    // Teste se dentro do objeto retornado, a função retorna uma key com value igual a área correta para um círculo de raio 3.
+    it('Teste se o objeto retornado tem 3 propriedades.', () => {
+      expect(Object.keys(circle(3)).length).toBe(3)
+    })
+    it('Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.', () => {
+      expect(circle()).toBeUndefined
+    })
+    it('Teste se dentro do objeto retornado, a função retorna uma key com value igual a circunferência correta para um círculo de raio 2.', () => {
+      expect(circle(2).circumference).toBeCloseTo(12.56) //correto = 12.566
+    });
+    it('Teste se dentro do objeto retornado, a função retorna uma key com value igual a área correta para um círculo de raio 3.', () => {
+      expect(circle(3).area).toBeCloseTo(28.259) //correto = 28.274
+    })
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
-});
+// });
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,13 +28,20 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
+    const produtos = productDetails('arroz', 'feijão');
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
+    expect(Array.isArray(produtos)).toBe('true');
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails('arroz', 'feijão').length).toBe(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof produtos[0] || produtos[1]).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(produtos[0]).not.toEqual(produtos[1]);
     // Teste se os dois productIds terminam com 123.
+    expect(produtos[0].details.productId.endsWith('123') && produtos[1].details.productId.endsWith('123')).toBe(true)
   });
 });
